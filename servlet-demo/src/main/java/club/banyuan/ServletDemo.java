457448848fa1603afaad28935041d66ddc4bdd4c
edz <edz@gxgdeMacBook-Air.local>
2020-04-29 18:52:24 +0800

    4.29

---
+++ servlet-demo/src/main/java/club/banyuan/ServletDemo.java
@@ -0,0 +1,21 @@
+package club.banyuan;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class ServletDemo extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.setContentType("text/html;charset=utf-8");
+        PrintWriter out = resp.getWriter();
+        out.println("<html>" +
+                "<body>" +
+                "<h1 align=center> 这是你手写的Servlet </h1>" +
+                "今天是4月22日" +
+                "</body>" + "<html>");
+    }
+}
